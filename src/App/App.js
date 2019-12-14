0cb8d74e069a2487879439aafcf6ff096ec29ce0
Callan Morrison <callan.l.morrison@gmail.com>
2019-12-14 11:43:28 -0600

    Viewing all boards by user id

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import firebase from 'firebase/app';
 import firebaseConnection from '../helpers/data/connection';
 import Auth from '../components/Auth/Auth';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
+import BoardsContainer from '../components/BoardsContainer/BoardsContainer'
 
 import './App.scss';
 
@@ -38,7 +39,7 @@ class App extends React.Component {
         {/* if they are authenticated, load the board */}
         {/* else show login button */}
         {
-          (authed) ? (<div>You logged in</div>) : (<Auth />)
+          (authed) ? (<BoardsContainer />) : (<Auth />)
         }
       </div>
     );
